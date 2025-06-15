b4b8230655af93059d985d5fb16fd9de99b43990
starnotes <starnotes@qq.com>
2025-06-15 13:28:55 +0800

    蜂鸣器

--- code-1.0/applications/sample/wifi-iot/app/code_v2.0/chapter_04/beeper_music_demo.c
+++ code-1.0/applications/sample/wifi-iot/app/code_v2.0/chapter_04/beeper_music_demo.c
@@ -1,4 +1,3 @@
-// Created by 星记 on 25-6-15.
 #include <stdio.h>
 #include <unistd.h>
 #include "ohos_init.h"
@@ -7,6 +6,8 @@
 #include "wifiiot_gpio_ex.h"
 #include "wifiiot_watchdog.h"
 #include "wifiiot_pwm.h"
+#include "hi_pwm.h"
+
 static volatile int g_buttonPressed = 0;
 static const uint16_t g_tuneFreqs[] = {
         0,     // 40M Hz 对应的分频系数：
@@ -34,32 +35,35 @@ static const uint8_t g_scoreDurations[] = {
 static void *BeeperMusicTask(const char *arg)
 {
     (void)arg;
-    printf("BeeperMusicTask start!\r\n");
-    hi_pwm_set_clock(PWM_CLK_XTAL); // 设置时钟源为晶体时钟（40MHz，默认时钟源160MHz）
-    while (1)
-    {
-        for (size_t i = 0; i < sizeof(g_scoreNotes) / sizeof(g_scoreNotes[0]); i++)
-        {
-            uint32_t tune = g_scoreNotes[i]; // 音符
-            uint16_t freqDivisor = g_tuneFreqs[tune];
-            uint32_t tuneInterval = g_scoreDurations[i] * (125 * 1000); // 音符时间
-            printf("%d %d %d %d\r\n", tune, (40 * 1000 * 1000) / freqDivisor, freqDivisor, tuneInterval);
-            PwmStart(WIFI_IOT_PWM_PORT_PWM0, freqDivisor / 2, freqDivisor);
-            usleep(tuneInterval);
-            PwmStop(WIFI_IOT_PWM_PORT_PWM0);
-        }
-    }
-    return NULL;
-}
-static void StartBeepMusicTask(void)
-{
-    osThreadAttr_t attr;
     GpioInit();
     // 蜂鸣器引脚 设置为 PWM功能
     IoSetFunc(WIFI_IOT_IO_NAME_GPIO_9, WIFI_IOT_IO_FUNC_GPIO_9_PWM0_OUT);
     PwmInit(WIFI_IOT_PWM_PORT_PWM0);
     WatchDogDisable();
 
+    printf("BeeperMusicTask start!\r\n");
+    hi_pwm_set_clock(PWM_CLK_XTAL); // 设置时钟源为晶体时钟（40MHz，默认时钟源160MHz）
+    while (1)
+    {
+        for (size_t i = 0; i < sizeof(g_scoreNotes) / sizeof(g_scoreNotes[0]); i++)
+        {
+            uint32_t tune = g_scoreNotes[i];                            // 音符
+            uint16_t freqDivisor = g_tuneFreqs[tune];                   //获取 频率
+            uint32_t tuneInterval = g_scoreDurations[i] * (125 * 1000); // 音符时间   ，节拍，
+            printf("%d %d %d %d\r\n", tune, (40 * 1000 * 1000) / freqDivisor, freqDivisor, tuneInterval);
+            PwmStart(WIFI_IOT_PWM_PORT_PWM0, freqDivisor / 2, freqDivisor);
+            usleep(tuneInterval); //持续响tuneInterval时间，
+            PwmStop(WIFI_IOT_PWM_PORT_PWM0);
+        }
+        usleep(2000 * 1000);
+    }
+    return NULL;
+}
+static void StartBeepMusicTask(void)
+{
+    osThreadAttr_t attr;
+
+    //设置新线程属性
     attr.name = "BeeperMusicTask";
     attr.attr_bits = 0U;
     attr.cb_mem = NULL;
@@ -67,9 +71,11 @@ static void StartBeepMusicTask(void)
     attr.stack_mem = NULL;
     attr.stack_size = 1024;
     attr.priority = osPriorityNormal;
+
+    //创建线程，在新的线程中运行BeeperMusicTask函数
     if (osThreadNew((osThreadFunc_t)BeeperMusicTask, NULL, &attr) == NULL)
     {
-        printf("[LedExample] Falied to create BeeperMusicTask!\n");
+        printf("[BeeperMusicTask] Falied to create BeeperMusicTask!\n");
     }
 }
-SYS_RUN(StartBeepMusicTask);
+APP_FEATURE_INIT(StartBeepMusicTask);
